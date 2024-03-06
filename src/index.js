c9f1cb99c89aab1d2e22c9c6908c26b8b24fef5c
Maheen <maheenharisaftab@yahoo.com>
2024-03-06 16:40:42 -0700

    24

--- src/index.js
+++ src/index.js
@@ -19,17 +19,6 @@ function Square(props) {
       };
     }
   
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-        });
-      }
 
     renderSquare(i) {
       return <Square
@@ -82,6 +71,22 @@ class Game extends React.Component {
       };
     }
   
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
