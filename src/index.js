5cf0207170148cd944ecc3dda2b12ab6bf4cfe69
Mehak <kaurmehak1007@gmail.com>
2024-03-06 16:29:47 -0700

    19

--- src/index.js
+++ src/index.js
@@ -21,12 +21,15 @@ function Square(props) {
   
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
           squares: squares,
           xIsNext: !this.state.xIsNext,
         });
-    }
+      }
 
     renderSquare(i) {
       return <Square
