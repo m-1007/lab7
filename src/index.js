67bf9614248b9383df025f3aa0c48b49309771c1
Mehak <kaurmehak1007@gmail.com>
2024-03-06 16:34:24 -0700

    21

--- src/index.js
+++ src/index.js
@@ -11,42 +11,35 @@ function Square(props) {
   }
   
   class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-    }
-  
     handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-        });
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
       }
-
-    renderSquare(i) {
-      return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)} />;
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
+    }
   
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
         status = 'Winner: ' + winner;
-        } else {
+      } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
+      }
   
       return (
         <div>
@@ -69,7 +62,7 @@ function Square(props) {
         </div>
       );
     }
-}
+  }
   
 class Game extends React.Component {
     constructor(props) {
